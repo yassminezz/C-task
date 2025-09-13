5ec9b9c8ae2a8f33842feed73f254203027fb31d
Yasmine <yassminramadan111@gmail.com>
2025-09-13 12:19:38 +0300

    day3Task

---
+++ Day3/Day3/Question.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    public class Question
+    {
+        public string Header { get; set; }
+        public string Body { get; set; }
+        public int Mark { get; set; }
+        public Question() { }
+
+        public Question(string header, string body, int mark)
+        {
+            Header = header;
+            Body = body;
+            Mark = mark;
+        }
+        public virtual void Show()
+        {
+            Console.WriteLine($"{Header} ({Mark} Marks)");
+            Console.WriteLine(Body);
+        }
+
+    }
+}
